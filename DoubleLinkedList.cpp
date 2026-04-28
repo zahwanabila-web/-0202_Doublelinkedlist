6f8edc822154ae84e567742c0abf08cf0484c3c4
ZahwaNabila <zahwanabila434@gmail.com>
2026-04-28 13:05:03 +0700

    commit5

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -38,8 +38,16 @@ public:
                 cout << "\nDuplicate number not allowed" << endl;
                 return;
             }
-
+            newNode->next = START;
+        
+            if (START != NULL)
+                START->prev = newNode;
             
+            newNode->prev = NULL;
+            
+            START = newNode;
+            return;
         }
+        
     }
 };
